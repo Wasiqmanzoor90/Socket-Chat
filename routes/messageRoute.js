29118ea6434ea0a7af65006d97f515d2ca950318
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2025-07-30 11:01:15 -0400

    c3

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -2,15 +2,15 @@ import express from 'express';
 import authMiddleware from '../middleware/auth.middleware.js';
 import { createMessage, getLatestMessages, getRoomMessages } from '../controller/messageController.js';
 
-//routes
 const router = express.Router();
 
 router.use(authMiddleware);
 router.post('/', createMessage);
-router.get('/:roomId', getRoomMessages);
+
+//  Specific route first
 router.get('/latest/:id', getLatestMessages);
 
+//  Generic route last
+router.get('/:roomId', getRoomMessages);
+
 export default router;
-
-
-
